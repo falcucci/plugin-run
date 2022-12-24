f22e741e3909b2992f903da83c597a1b7c84f161
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-12-24 17:26:09 +0100

    add the code to execute step by step
    this commit adds the code to execute step by step.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Initialize the app and start the server');
+console.log("Initialize the app and start the server");
+
+const fs = require("fs");
+
+// import the code to execute step by step
+const code = fs.readFileSync("./code.js", "utf8").toString();
+
+steps = [
+  {
+    name: "step1",
+    code: code,
+  },
+];
+
