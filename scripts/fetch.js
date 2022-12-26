0c2f02d33add996cb726250c3fd25e1feecd0571
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-12-26 17:34:48 +0100

    fix fetch.js
    the api_url variable was not being used in the fetch.js script.

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,6 +1,6 @@
 const fetch = require('fetch');
 
-// const { API_URL } = process.env;
+const { API_URL } = process.env;
 
 const response = fetch(API_URL).then(response => {
   return response.json().then(data => {
