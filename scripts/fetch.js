e2114963bec7302fba9f4ee18d7ef108ba321466
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-12-26 17:34:32 +0100

    feat: add health endpoint
    this commit adds a health endpoint to the api.
    
    the health endpoint is used to check if the api is up and running.

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,7 +1,6 @@
 const fetch = require('fetch');
 
 // const { API_URL } = process.env;
-const API_URL = "https://pokedex-falcucci.vercel.app/api/health";
 
 const response = fetch(API_URL).then(response => {
   return response.json().then(data => {
